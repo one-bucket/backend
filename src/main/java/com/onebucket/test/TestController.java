0ae1bfa6c065364a60f329b3b7dfe3548254ad33
unikal1 <49333855+unikal1@users.noreply.github.com>
2024-04-01 22:24:14 +0900

    Feat: 테스트 용도 controller 추가(TestController)
    "/test" 엔드포인트에 대한 커밋 당시 날짜를 포함한 응답 바디 추가. ("test at 24-04-01")
    
    SecurityConfig 추가 필

--- src/main/java/com/onebucket/test/TestController.java
+++ src/main/java/com/onebucket/test/TestController.java
@@ -2,7 +2,6 @@ package com.onebucket.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
