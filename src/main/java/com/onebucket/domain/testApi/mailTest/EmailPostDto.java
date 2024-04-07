0a02540f4311ac1965b1bca33694d479a0853849
unikal1 <49333855+unikal1@users.noreply.github.com>
2024-04-07 16:58:36 +0900

    Feature: google email sender test api 작성

---
+++ src/main/java/com/onebucket/domain/testApi/mailTest/EmailPostDto.java
@@ -0,0 +1,15 @@
+package com.onebucket.domain.testApi.mailTest;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailPostDto {
+
+    private String email;
+}
---
+++ src/main/java/com/onebucket/domain/testApi/mailTest/EmailService.java
@@ -0,0 +1,64 @@
+package com.onebucket.domain.testApi.mailTest;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.util.Random;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+    private final SpringTemplateEngine springTemplateEngine;
+
+    public String sendMail(EmailMessageEntity emailMessageEntity,String type) {
+        String authNum = createCode();
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false);
+            mimeMessageHelper.setTo(emailMessageEntity.getTo()); // 메일 수신자
+            mimeMessageHelper.setSubject(emailMessageEntity.getSubject()); // 메일 제목
+            mimeMessageHelper.setText(setContext(authNum, type), true); // 메일 본문 내용, HTML 여부
+            javaMailSender.send(mimeMessage);
+
+            log.info("Success");
+            return authNum;
+        } catch (MessagingException e) {
+            log.info("fail");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String createCode() {
+        Random random = new Random();
+        StringBuffer key = new StringBuffer();
+
+        for (int i = 0; i < 8; i++) {
+            int index = random.nextInt(4);
+
+            switch (index) {
+                case 0: key.append((char) ((int) random.nextInt(26) + 97)); break;
+                case 1: key.append((char) ((int) random.nextInt(26) + 65)); break;
+                default: key.append(random.nextInt(9));
+            }
+        }
+        return key.toString();
+    }
+
+    public String setContext(String code, String type) {
+        Context context = new Context();
+        context.setVariable("code", code);
+        return springTemplateEngine.process(type, context);
+    }
+}
+
